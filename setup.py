5b8f5466dd377c26496dc4de49b67155fab6157e
Christophe Boulanger <christophe.boulanger@imio.be>
2022-02-03 09:22:57 +0100

    Back to development: 2.0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.sendinblue",
-    version="2.0.2",
+    version="2.0.3.dev0",
     description="Sendinblue integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
