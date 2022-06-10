c4b14f6ede63c4913ae32a9cced4f4a1d6c8bcd7
Laurent Lasudry <lasudry@gmail.com>
2022-06-10 15:38:01 +0200

    Preparing release 1.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.sendinblue',
-    version='1.0.5.dev0',
+    version='1.0.5',
     description="Sendinblue integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
